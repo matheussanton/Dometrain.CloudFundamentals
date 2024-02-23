cdecc4e4071768197c9e96abe545418499d574d3
Matheus Santon <matheussantong@gmail.com>
2024-02-23 00:20:23 -0300

    AWS SQS: 4/14

--- 3.Sqs/SQS.Publisher/Program.cs
+++ 3.Sqs/SQS.Publisher/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SQS.Publisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "matheussanton@gmail.com",
+    FullName = "Matheus Santon",
+    DateOfBirth = new DateTime(1995, 10, 10),
+    GitHubUsername = "matheussanton"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new  Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType",
+            new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
