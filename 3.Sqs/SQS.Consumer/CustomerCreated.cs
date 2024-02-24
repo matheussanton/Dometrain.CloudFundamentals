1b14ed914d279529e1430774f362db01f0b3a29e
Matheus Santon <matheussantong@gmail.com>
2024-02-24 15:45:15 -0300

    AWS SQS: 5/14

---
+++ 3.Sqs/SQS.Consumer/CustomerCreated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQS.Consumer
+{
+    public class CustomerCreated
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string GitHubUsername { get; set; }
+    }
+}
